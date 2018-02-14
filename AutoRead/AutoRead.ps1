76d23d53b0fc9626243d57e44c18ccbc1ba97d0c
0x237 <win_lucky@163.com>
2018-02-14 13:03:35 +0800

    show hours

--- AutoRead/AutoRead.ps1
+++ AutoRead/AutoRead.ps1
@@ -5,7 +5,7 @@ adb connect 127.0.0.1:62001
 
 
 $flag=0 #阅读总时长计时
-while($flag -lt 10800000)
+while($flag -lt 18800000)
 {
 	$dx=Get-Random -minimum 621 -maximum 651
 	$dy=Get-Random -minimum 913 -maximum 953
@@ -13,5 +13,6 @@ while($flag -lt 10800000)
 	$t = Get-Random -minimum 30000 -maximum 50000
 	Start-Sleep -m $t #阅读30-50秒
 	$flag=[Int]$flag+[Int]$t
-	echo $flag
+	$hour=[Int]$flag/3600000
+	echo $hour
 }
